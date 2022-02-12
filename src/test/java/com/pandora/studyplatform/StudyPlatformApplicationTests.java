aee5a0cbd2bebc48c3d8be0a7a4805937f83afb4
Pandora <1746384966@qq.com>
2022-02-12 20:16:09 +0800

    完成了项目的创建，框架的搭建，并删除了一些不必要的文件。

---
+++ src/test/java/com/pandora/studyplatform/StudyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandora.studyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
